0385ae6d604e09f6a64e20525a450aa166c179df
ashishkumarmurmusit <ashishkumarmurmusit256@gmail.com>
2024-05-13 19:41:43 +0530

    Update scriptkey.js

--- scriptkey.js
+++ scriptkey.js
@@ -3,7 +3,7 @@ function checkPassword() {
     var key = "your_password_here"; // Replace "your_password_here" with your actual password
 
     if (password === key) {
-        window.location.href = "./minesziverd/index.html"; // Redirect to the next page
+        window.location.href = "./mine.html"; // Redirect to the next page
     } else {
         document.getElementById("error-message").innerText = "Incorrect password. Please try again.";
     }
